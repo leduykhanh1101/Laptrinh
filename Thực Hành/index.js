60df934d836682749e940c933b68991a4108a27d
leduykhanh1101 <nhocnano2@gmail.com>
2025-11-04 09:35:36 +0700

    java

---
+++ Thực Hành/index.js
@@ -0,0 +1,21 @@
+// var BMI=28
+// if (
+//     BMI < 18.5
+// ) { console.log("Gầy");}
+// if (
+//     BMI >= 18.5 && BMI < 24.9
+// ) {console.log("Bình Thường");}
+// if (
+//     BMI >= 25 && BMI < 29.9
+// ) {console.log("Thừa Cân");}
+// if (
+//     BMI >= 30
+// ) {    console.log("Béo Phì"); }
+
+var x=2025
+var y=4
+var z=100
+if (x % y ===0 || x % z ===0) {console.log("Năm Nhuận");
+}
+else {console.log("Năm Nghịch");
+}
