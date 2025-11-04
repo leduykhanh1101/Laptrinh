d9dc5a7551e3e720eb21c002747bc749df6c0ba9
leduykhanh1101 <nhocnano2@gmail.com>
2025-11-04 09:42:19 +0700

    Update index.js

--- Thực Hành/index.js
+++ Thực Hành/index.js
@@ -1,16 +1,16 @@
-// var BMI=28
-// if (
-//     BMI < 18.5
-// ) { console.log("Gầy");}
-// if (
-//     BMI >= 18.5 && BMI < 24.9
-// ) {console.log("Bình Thường");}
-// if (
-//     BMI >= 25 && BMI < 29.9
-// ) {console.log("Thừa Cân");}
-// if (
-//     BMI >= 30
-// ) {    console.log("Béo Phì"); }
+var BMI=28
+if (
+    BMI < 18.5
+) { console.log("Gầy");}
+if (
+    BMI >= 18.5 && BMI < 24.9
+) {console.log("Bình Thường");}
+if (
+    BMI >= 25 && BMI < 29.9
+) {console.log("Thừa Cân");}
+if (
+    BMI >= 30
+) {    console.log("Béo Phì"); }
 
 var x=2025
 var y=4
